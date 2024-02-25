1092ea3eedb29cc49316b1e08854ade28a77fb3a
Szymon Hankus <dephan@protonmail.com>
2024-02-25 15:47:32 +0100

    put all the icons into separate components

--- src/components/TheInbox.vue
+++ src/components/TheInbox.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { onMounted, ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
 import { getEmails } from '../api/ApiClient'
 import type { Address, EmailPreview } from '@/common/models';
-import PreviewList from './PreviewList.vue';
-import { useRouter } from 'vue-router';
 import { generateNewAddress, saveAddressToStorage } from '@/common/AddressUtils';
+import ClipboardIcon from './icons/ClipboardIcon.vue';
+import RefreshIcon from './icons/RefreshIcon.vue';
+import PreviewList from './PreviewList.vue';
 
 interface Props {
   address: Address;
@@ -24,7 +26,7 @@ const fetchEmails = async () => {
 const newAddress = () => {
   const newAddr = generateNewAddress();
   saveAddressToStorage(newAddr);
-  router.push({name: "inbox", query: {login: newAddr.login, domain: newAddr.domain} })
+  router.push({name: "inbox", query: {login: newAddr.login, domain: newAddr.domain}})
 }
 
 watch(props, () => {
@@ -41,15 +43,11 @@ onMounted(() => { fetchEmails() })
           <span id="address">{{ `${props.address.login}@${props.address.domain}` }}</span>
           <div class="separator"></div>
           <button class="svg-button" id="clipboard-button" @click="copyAddressToClipboard()">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
-            </svg>
+            <ClipboardIcon class="w-6 h-6"/>
           </button>
         </div>
         <button class="svg-button" id="refresh-button" @click="fetchEmails()">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
-          </svg>
+            <RefreshIcon class="w-6 h-6"></RefreshIcon>
         </button>
       </div>
       <button @click="newAddress()">New address</button>
